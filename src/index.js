a8968df8db638e5408e7bc866e555d08784ee850
Simon Hoyos <shoyos@helloelephant.com>
2018-06-21 16:49:58 -0500

    Add accordion functionality

--- src/index.js
+++ src/index.js
@@ -8,4 +8,21 @@ import initHealth from './Health/index';
   initLanding();
   initHealth();
   initModal();
+
+  const accordionButtons = document.querySelectorAll('.accordion__button');
+
+  function toggleAccordion(el) {
+    el.addEventListener('click', function() {
+      this.classList.toggle('accordion__panel-active');
+
+      const panel = this.nextElementSibling;
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'block';
+      }
+    });
+  }
+
+  accordionButtons.forEach(toggleAccordion);
 })();
