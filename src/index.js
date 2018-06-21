f48fee2f00f3096da744399ee81d1291db6f0c2a
danielcb29 <danielc.web@gmail.com>
2018-06-21 16:16:44 -0500

    Moving modal logic to main app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import initEmployment from './Employment';
 import initLanding from './Landing';
-import initModal from './Modal/index';
 import initHealth from './Health/index';
+
+function initModal() {
+  const MODAL_CLASS = 'modal';
+  const modal = document.querySelector(`.${MODAL_CLASS}`);
+  const openModalButton = document.querySelector(`.${MODAL_CLASS}--open`);
+  const closeModalButton = document.querySelector(`.${MODAL_CLASS}--close`);
+
+  const openCloseModal = () => {
+    modal.classList.toggle(`${MODAL_CLASS}--active`);
+  };
+
+  openModalButton.addEventListener('click', openCloseModal);
+  closeModalButton.addEventListener('click', openCloseModal);
+} 
 
 (function App() {
   initEmployment();
