08d3e51417fbbf7bb77e65b6993f0be157a1d247
danielcb29 <danielc.web@gmail.com>
2018-06-21 12:19:32 -0500

    Adding modal component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-const root = document.querySelector('.root');
+import initModal from './Modal/index';
 
-root.innerHTML = '<h1>Hello world</h1>';
+import healthTemplate from './Salud/index.html';
+import initHealth from './Salud/index';
+
+const root = document.querySelector('.root');
+root.innerHTML = healthTemplate;
+
+initModal();
+
+initHealth();
